9a38557248404d0acf0098dfa6a0037464fc5b48
unname666 <78314985+unname666@users.noreply.github.com>
2021-03-17 17:21:05 -0700

    working on it

--- index.js
+++ index.js
@@ -1,10 +1,57 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const fs = require('fs')
+const path = require('path');
+const generateMarkdown = require('./generateMarkdown.js')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'what is your Github username?',
+        name: 'username',
+    },
+    {
+        type: 'input',
+        message: 'what is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'what does the user have to  install before working with this app?',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'tell me about the project',
+        name: 'description',
+    },
+    {
+        type: 'list',
+        message: 'any license?',
+        name: 'license',
+        choices: ["IBM","MIT","none"],
+    },
+    {
+        type: 'input',
+        message: 'what is you app going to be used for?',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'any contribution guidelines anyone needs to follow?',
+        name: 'contribution',
+    },
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFileSync('readme.md', data, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+      });
+}
 
 // TODO: Create a function to initialize app
 function init() {}
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,20 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== "none"){
+    // https://img.shields.io/badge/License-IPL%201.0-blue.svg
+    return `![github license]https://img.shields.io/badge/License-${license}-blue.svg`
+  }return ""
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== "none"){
+    
+    return `* [License](#license)`
+  }return ""
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -13,7 +23,22 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
+  ${renderLicenseLink(data.license)}
+  * [table of contents](#table-of-contents)
+  * [installation](#installation)
+  * [description](#description)
+  * [usage](#usage)
+      *[contribution](#contribution)
+  ## installation \n ${data.installation}
 
+  ## description \n ${data.description}
+
+  ## usage \n ${data.usage}
+
+  ## contribution\n ${data.contribution}
+
+  <small><i>If you have anyquestions vist my github<a href='https://github.com/ScottLRay/Readme-generator'>Scott Ray</a> or email me at ... </i></small>
 `;
 }
 
